d2824073d841b80d8548551c9d07c81af41b60ac
fabriziovz <fabrizio.vasquez@utec.edu.pe>
2019-06-03 00:13:35 -0500

    CRUDM

--- web/server.py
+++ web/server.py
@@ -17,6 +17,7 @@ def static_content(content):
     return render_template(content)
 
 
+
 @app.route('/users', methods = ['GET'])
 def get_users():
     session = db.getSession(engine)
@@ -26,8 +27,7 @@ def get_users():
         data.append(user)
     return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
 
-
-"""@app.route('/users/<id>', methods = ['GET'])
+@app.route('/users/<id>', methods = ['GET'])
 def get_user(id):
     db_session = db.getSession(engine)
     users = db_session.query(entities.User).filter(entities.User.id == id)
@@ -37,64 +37,125 @@ def get_user(id):
 
     message = { 'status': 404, 'message': 'Not Found'}
     return Response(message, status=404, mimetype='application/json')
-"""
-
-@app.route('/create_test_users', methods = ['GET'])
-def create_test_users():
-    db_session = db.getSession(engine)
-    user = entities.User(name="David", fullname="Lazo", password="1234", username="qwerty")
-    db_session.add(user)
-    db_session.commit()
-    return "Test user created!"
 
 
 @app.route('/users', methods = ['POST'])
 def create_user():
-    c =  json.loads(request.form['values'])
-    user = entities.User(
-        username=c['username'],
-        name=c['name'],
-        fullname=c['fullname'],
-        password=c['password']
-    )
-    session = db.getSession(engine)
-    session.add(user)
-    session.commit()
-    return 'Created User'
+        c =  json.loads(request.form['values'])
+        user = entities.User(
+            username=c['username'],
+            name=c['name'],
+            fullname=c['fullname'],
+            password=c['password']
+            )
+        session = db.getSession(engine)
+        session.add(user)
+        session.commit()
+        return 'Created User'
 
-"""
-@app.route('/users', methods = ['GET'])
-def get_users():
+
+@app.route('/users', methods = ['PUT'])
+
+
+def update_user():
+        session = db.getSession(engine)
+        id = request.form['key']
+        user = session.query(entities.User).filter(entities.User.id == id).first()
+        c =  json.loads(request.form['values'])
+        for key in c.keys():
+            setattr(user, key, c[key])
+        session.add(user)
+        session.commit()
+        return 'Updated User'
+
+
+
+@app.route('/messages', methods = ['POST'])
+def create_message():
+        c =  json.loads(request.form['values'])
+        message = entities.Message(
+            content=c['content'],
+            user_from_id=c['user_from_id'],
+            user_to_id=c['user_to_id']
+            )
+        session = db.getSession(engine)
+        session.add(message)
+        session.commit()
+        return 'Created Message'
+
+
+@app.route('/messages', methods = ['GET'])
+def get_messages():
     session = db.getSession(engine)
-    dbResponse = session.query(entities.User)
-    data = dbResponse[:]
+    dbResponse = session.query(entities.Message)
+    data = []
+    for message in dbResponse:
+        data.append(message)
     return Response(json.dumps(data, cls=connector.AlchemyEncoder), mimetype='application/json')
 
-"""
-@app.route('/users', methods = ['PUT'])
-def update_user():
-    session = db.getSession(engine)
-    id = request.form['key']
-    user = session.query(entities.User).filter(entities.User.id == id).first()
-    c =  json.loads(request.form['values'])
-    for key in c.keys():
-        setattr(user, key, c[key])
-    session.add(user)
-    session.commit()
-    return 'Updated User'
+
+
+@app.route('/messages/<id>', methods = ['GET'])
+def get_message(id):
+    db_session = db.getSession(engine)
+    messages = db_session.query(entities.Message).filter(entities.Message.id == id)
+    for message in messages:
+        js = json.dumps(message, cls=connector.AlchemyEncoder)
+        return  Response(js, status=200, mimetype='application/json')
+
+    message = { 'status': 404, 'message': 'Not Found'}
+    return Response(message, status=404, mimetype='application/json')
+
+
+@app.route('/messages', methods = ['PUT'])
+def update_message():
+        session = db.getSession(engine)
+        id = request.form['key']
+        user = session.query(entities.Message).filter(entities.Message.id == id).first()
+        c =  json.loads(request.form['values'])
+        for key in c.keys():
+            setattr(user, key, c[key])
+        session.add(user)
+        session.commit()
+        return 'Updated Message Form'
+
+
 
 @app.route('/users', methods = ['DELETE'])
+def delete_user():
+        id = request.form['key']
+        session = db.getSession(engine)
+        messages = session.query(entities.User).filter(entities.User.id == id).one()
+        session.delete(messages)
+        session.commit()
+        return "Deleted Message"
+
+
+@app.route('/messages', methods = ['DELETE'])
 def delete_message():
-    id = request.form['key']
-    session = db.getSession(engine)
-    messages = session.query(entities.User).filter(entities.User.id == id)
-    for message in messages:
-        session.delete(message)
-    session.commit()
-    return "Deleted Message"
+        id = request.form['key']
+        session = db.getSession(engine)
+        messages = session.query(entities.Messages).filter(entities.Messages.id == id).one()
+        session.delete(messages)
+        session.commit()
+        return "Deleted Message"
 
 
+@app.route('/authenticate',methods=['POST'])
+def authenticate():
+    username=request.form['username']
+    password=request.form['password']
+    db_session=db.getSession(engine)
+
+    try:
+        user=db_session.query(entities.User
+            ).filter(entities.User.username==username
+            ).filter(entities.User.password==password
+            ).one()
+        return render_template("success.html")
+    except Exception:
+        return render_template("fail.html")
 
 if __name__ == '__main__':
     app.secret_key = ".."
-    app.run(port=8080, threaded=True, host=('127.0.0.1'))
+    app.run(port=8080, threaded=True, host=('127.0.0.1'),debug=True)
